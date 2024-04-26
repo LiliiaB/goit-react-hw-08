380bb3006405f447b644cb8a26d0c00c7513c8e2
Lilia Levchenko <liliya.benatti@gmail.com>
2024-04-26 21:27:50 +0200

    routes added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,80 @@
-import "./App.css";
-import { ContactForm } from "../ContactForm/ContactForm";
-import { SearchBox } from "../SearchBox/SearchBox";
-import { ContactList } from "../ContactList/ContactList";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { fetchContacts } from "../../redux/contactsOps";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Layout } from "./Layout/Layout";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, lazy } from "react";
+import { refreshUser } from "redux/auth/operations";
+import { PublicRoute } from "./PublicRoute";
+import { PrivatRoute } from "./PrivateRoute";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { getIsRefreshing } from "redux/auth/selectors";
 
-export default function App() {
+const Home = lazy(() => import("Pages/Home/Home"));
+const ContactDetails = lazy(() =>
+  import("Pages/ContactDetails/ContactDetails")
+);
+const ContactsPage = lazy(() => import("Pages/ContactsPage/ContactsPage"));
+const RegistrationPage = lazy(() =>
+  import("Pages/RegistrationPage/RegistrationPage")
+);
+const LogInPage = lazy(() => import("Pages/LoginPage/LogInPage"));
+
+export const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(refreshUser());
   }, [dispatch]);
 
+  const isRefreshing = useSelector(getIsRefreshing);
+
   return (
-    <>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      <ContactList />
-    </>
+    <div
+      style={{
+        height: "100vh",
+        fontSize: 20,
+        color: "#010101",
+        textAlign: "center",
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute redirectTo="/contacts" component={<LogInPage />} />
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <PublicRoute
+                redirectTo="/contacts"
+                component={<RegistrationPage />}
+              />
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivatRoute redirectTo="/login" component={<ContactsPage />} />
+            }
+          >
+            <Route
+              path=":id"
+              element={
+                <PrivatRoute
+                  redirectTo="/login"
+                  component={<ContactDetails />}
+                />
+              }
+            />
+          </Route>
+          <Route path="*" element={<Navigate to="/" replace={true} />} />
+        </Route>
+      </Routes>
+      <ToastContainer />
+    </div>
   );
-}
+};
