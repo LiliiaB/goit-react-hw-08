a51e64a93c534825535ccba86a4d730409b165c8
Lilia Levchenko <liliya.benatti@gmail.com>
2024-04-26 21:34:41 +0200

    structure added

---
+++ src/components/UserMenu/UserMenu.js
@@ -0,0 +1,16 @@
+import { useDispatch, useSelector } from "react-redux";
+import { logOut } from "redux/auth/operations";
+import { getUser } from "redux/auth/selectors";
+import { Button, ContainerMenu } from "./UserMenu.styled";
+
+export const UserMenu = () => {
+  const user = useSelector(getUser);
+  const dispatch = useDispatch();
+
+  return (
+    <ContainerMenu>
+      <p>{user.name}</p>
+      <Button onClick={() => dispatch(logOut())}>Log out</Button>
+    </ContainerMenu>
+  );
+};
